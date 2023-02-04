1753098645180d568f251fd57ddff40280ccac24
gitHwan <moongodd88@gmail.com>
2023-02-04 22:06:16 +0900

    Test Commit

---
+++ pages/01-01-qqq/index.js
@@ -0,0 +1,15 @@
+import styles from '@/styles/Home.module.css'
+
+export default function Qqq() {
+    // javascript 영역
+
+    // html 영역
+    return(
+        <div>
+            <div>철수</div>
+            <button>클릭하세요!</button>
+        </div>
+    )
+
+
+}
---
+++ styles/emotion.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled"
+
+// 반드시 대문자
+export const Email = styled.div`
+    color: red;
+`
+
+export const EmailInput = styled.input`
+    width: 500px;
+`
